29e8d4f4d04ac12c46cf1fe60db1d3d11149d0f9
Minseok-Choi <cms05041@gmail.com>
2021-12-05 16:40:28 +0900

    feat : input 문자열을 받았을 때 point 리스트로 변환하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String INPUT_ORDER = "좌표를 입력하세요.";
     private static final String POINT_REGEX = "\\(([0-9]{1,2}),([0-9]{1,2})\\)";
+    private static final String INPUT_REGEX = "(\\([0-9]{1,2},[0-9]{1,2}\\))(-(\\([0-9]{1,2},[0-9]{1,2}\\))){0,3}";
 
     public static void inputOrder() {
         System.out.println(INPUT_ORDER);
@@ -27,5 +28,27 @@ public class InputView {
             return new Point(x,y);
         }
         throw new IllegalArgumentException();
+    }
+
+    public static List<Point> generatePoints(String input) {
+        checkValidInput(input);
+        String[] splited = input.split("-");
+        List<Point> points = new ArrayList<>();
+        for (String s : splited) {
+            points.add(generatePoint(s));
+        }
+        return points;
+    }
+
+    private static void checkValidInput(String input) {
+        Pattern pattern = Pattern.compile(INPUT_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if(!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static double getAnswer(List<Point> points) {
+        return points.get(0).getDistance(points.get(1));
     }
 }
--- src/test/java/step2test/InputViewTest.java
+++ src/test/java/step2test/InputViewTest.java
@@ -2,11 +2,13 @@ package step2test;
 
 import domain.Point;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import view.InputView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputViewTest {
@@ -18,8 +20,25 @@ public class InputViewTest {
 
     @Test
     @DisplayName("좌표를 검증하고 Point로 생성한다.")
-    void split_input() {
+    void generatePoint() {
         String input = "(10,10)";
         Assertions.assertThat(InputView.generatePoint(input)).isEqualTo(new Point(10,10));
+    }
+
+    @Test
+    @DisplayName("인풋을 검증하고 List<Point>로 반환한다.")
+    void generatePoints() {
+        String input = "(10,10)-(11,11)";
+        List<Point> points = Arrays.asList(new Point(10,10), new Point(11,11));
+        Assertions.assertThat(InputView.generatePoints(input)).isEqualTo(points);
+    }
+
+    @Test
+    @DisplayName("point 리스트에서 거리를 계산하여 반환한다.")
+    void generatePoints_getDistance() {
+        String input = "(10,10)-(11,11)";
+        List<Point> list = InputView.generatePoints(input);
+        double result = list.get(0).getDistance(list.get(1));
+        Assertions.assertThat(InputView.getAnswer(list)).isEqualTo(result);
     }
 }
